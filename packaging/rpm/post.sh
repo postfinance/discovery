f15a4e813c3e8295a35bf130c99921e0e0772c2a
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-02-12 12:54:07 +0100

    fix: add exporter and discovery systemd files

--- packaging/rpm/post.sh
+++ packaging/rpm/post.sh
@@ -2,4 +2,5 @@
 systemctl daemon-reload
 if [ $1 -ge 1 ]; then
   systemctl try-restart discovery.service
+  systemctl try-restart exporter.service
 fi
--- packaging/rpm/pre.sh
+++ packaging/rpm/pre.sh
@@ -1,4 +1,6 @@
  if [ $1 -eq 0 ]; then
     systemctl stop discovery.service
+    systemctl stop exporter.service
     systemctl disable discovery.service
+    systemctl disable exporter.service
 fi
