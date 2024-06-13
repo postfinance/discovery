96038768cc41bf843fb48e089ad17a99edc365f8
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-06-13 13:13:46 +0200

    chore: update

--- tools.go
+++ tools.go
@@ -1,10 +1,9 @@
 //go:build tools
 // +build tools
 
-package proto
+package discovery
 
 import (
 	_ "connectrpc.com/connect"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
